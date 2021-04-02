7cc30892fdb8fde053896c7036398121ee8578a4
Nelson Foltz <nelsfoltz@gmail.com>
2021-04-02 15:36:43 -0400

    add: global styles using createGlobalStyle and adding Google fonts

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,24 @@
+import { Button } from 'components/common';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: white;
+    min-height: 100vh;
+    margin: 0;
+    color: black;
+    font-family: 'Kaushan Script'
+  }
+`;
+
 function App() {
-  return <h1>App</h1>;
+  return (
+    <>
+      <GlobalStyle />
+      <h1>App</h1>
+      <Button>Test</Button>
+    </>
+  );
 }
 
 export default App;
