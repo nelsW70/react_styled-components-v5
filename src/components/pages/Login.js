d36ccef3412c7c24103928030f2cb1b1ae91c4a5
Nelson Foltz <nelsfoltz@gmail.com>
2021-04-03 12:56:50 -0400

    add: interpolating functions in style logic, proptypes for Button component

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -22,6 +22,14 @@ const Form = styled.form`
     text-align: center;
     margin: 10px 0;
   }
+
+  > ${Button}:first-of-type {
+    margin-top: 40px;
+  }
+
+  > ${Input} {
+    margin-top: 20px;
+  }
 `;
 
 let timeout;
@@ -62,6 +70,7 @@ export default function Login() {
           <Spinner />
         ) : (
           <>
+            <span>Login if you have an account</span>
             <Input
               value={formFields.username}
               onChange={handleInputChange}
