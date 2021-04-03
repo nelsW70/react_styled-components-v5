df783f2bef7c15280f90e5f6ba25af531b8c5415
Nelson Foltz <nelsfoltz@gmail.com>
2021-04-03 07:46:46 -0400

    add: creating the input styled component

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,5 +2,19 @@ import React from 'react';
 import { PageLayout } from 'components/common';
 
 export default function Home() {
-  return <PageLayout>Home</PageLayout>;
+  return (
+    <PageLayout>
+      <h1>Home</h1>
+      <p>
+        I'm baby bespoke pour-over bicycle rights tbh fashion axe try-hard.
+        Migas narwhal direct trade gochujang twee master cleanse quinoa
+        meditation chicharrones. Gochujang kombucha palo santo kale chips.
+        Selvage jianbing sustainable letterpress kitsch irony tote bag. Cliche
+        selvage pop-up, subway tile waistcoat marfa small batch ramps
+        microdosing put a bird on it. Succulents schlitz selvage sriracha yuccie
+        glossier paleo narwhal unicorn, everyday carry before they sold out
+        pitchfork.
+      </p>
+    </PageLayout>
+  );
 }
